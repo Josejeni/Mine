30b6710be4c967720054efa1d3b7f4618bfb5c99
JosephinjeniferS <josephin@dbcyelagiri.edu.in>
2024-03-23 12:25:54 +0530

    Second commit

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import '../src/style.css'
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    
+    <>
+    <div className='container'>
+        <div className='card'>
+            <div className='card-body'> 
+             Hi Sachin..... </div>
+          
+        </div>
+    </div>
+    </>
   )
 }
 
